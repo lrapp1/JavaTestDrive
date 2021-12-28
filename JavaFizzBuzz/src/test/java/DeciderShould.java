401aceaaafb2d7acc7bc3c77db41939a0939198d
Laura Rapp <rapp.laurad@gmail.com>
2021-12-27 19:51:08 -0500

    Method names changed to match ind outputs, added input 20=bUZZ

--- JavaFizzBuzz/src/test/java/DeciderShould.java
+++ JavaFizzBuzz/src/test/java/DeciderShould.java
@@ -6,8 +6,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class DeciderShould {
 
     @Test
-    @DisplayName("Low value starting test")
-    void clientThree() {
+    @DisplayName("number divisible by 3")
+    void decideThreeIsFizz() {
         // arrange test conditions
         Decider systemUnderTest = new Decider();
 
@@ -18,13 +18,37 @@ class DeciderShould {
         assertEquals("Fizz", result);
     }
     @Test
-    @DisplayName("Low value starting test")
-    void clientFive() {
+    @DisplayName("number divisible by 5")
+    void decideFiveIsBuzz() {
         // arrange test conditions
         Decider systemUnderTest = new Decider();
 
         // act
         String result = systemUnderTest.decide(5);
+
+        // assert
+        assertEquals("Buzz", result);
+    }
+    @Test
+    @DisplayName("Another number divisible by three (9)")
+    void decideNineIsFizz() {
+        // arrange test conditions
+        Decider systemUnderTest = new Decider();
+
+        // act
+        String result = systemUnderTest.decide(9);
+
+        // assert
+        assertEquals("Fizz", result);
+    }
+    @Test
+    @DisplayName("Another number divisible by three (20)")
+    void decideTwentyIsBuzz() {
+        // arrange test conditions
+        Decider systemUnderTest = new Decider();
+
+        // act
+        String result = systemUnderTest.decide(20);
 
         // assert
         assertEquals("Buzz", result);
